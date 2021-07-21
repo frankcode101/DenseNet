22ea189f00089778fb695587bdeb7bc523a18357
frankcode101 <58405091+frankcode101@users.noreply.github.com>
2021-07-21 23:56:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ def load_model():
 with st.spinner('Model is being loaded..'):
   model=load_model()
 st.write("""
-         # Flower Classification
+         # Fashion MNIST Classification
          """
          )
-file = st.file_uploader("Please upload an brain scan file", type=["jpg", "png"])
+file = st.file_uploader("Please upload an image", type=["jpg", "png"])
 import cv2
 from PIL import Image, ImageOps
 import numpy as np
@@ -21,20 +21,24 @@ def import_and_predict(image_data, model):
         size = (28,28)
         image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
         image = np.asarray(image)
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
-        img_reshape = img[np.newaxis,...]
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        img_resize = (cv2.resize(img, dsize=(28, 28),interpolation=cv2.INTER_CUBIC))/255.
+        img_reshape = img_resize[np.newaxis,...]
         prediction = model.predict(img_reshape)
         return prediction
 if file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(file)
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag',
+                   'Ankle boot']
     st.image(image, use_column_width=True)
     predictions = import_and_predict(image, model)
     score = tf.nn.softmax(predictions[0])
-    st.write(predictions)
-    st.write(score)
+    st.write(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
     print(
     "This image most likely belongs to {} with a {:.2f} percent confidence."
     .format(class_names[np.argmax(score)], 100 * np.max(score))
